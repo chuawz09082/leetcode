2d0f8570abe2ba6fd1ec55fba3280ffc21f5e313
chuawz09082 <chuawz09082@gmail.com>
2024-11-13 07:00:38 -0500

    Time: 3 ms (49.34%) | Memory: 18.2 MB (29.91%) - LeetSync

---
+++ 113-path-sum-ii/path-sum-ii.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        if not root:
+            return []
+        
+        current = [[root,[root.val]]]
+        result = []
+
+        while current:
+            nxtlevel = []
+            for i in range(len(current)):
+                node = current[i][0]
+                listvals = current[i][1]
+                if not node.left and not node.right and sum(listvals) == targetSum:
+                    result.append(listvals)
+                if node.left:
+                    nxtlevel.append([node.left,listvals + [node.left.val]])
+                if node.right:
+                    nxtlevel.append([node.right,listvals + [node.right.val]])
+            current = nxtlevel
+        
+        return result
+        
+            
+
+        
