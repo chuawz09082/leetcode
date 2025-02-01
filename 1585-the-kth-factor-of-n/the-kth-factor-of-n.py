b753e6a2dfc2a7fa345578255b55844d4ec2e5e2
chuawz09082 <chuawz09082@gmail.com>
2025-02-01 17:03:41 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (21.14%) - LeetSync

--- 1585-the-kth-factor-of-n/the-kth-factor-of-n.py
+++ 1585-the-kth-factor-of-n/the-kth-factor-of-n.py
@@ -8,9 +8,11 @@ class Solution:
             # Check if num is a factor of n (i.e., divides n without leaving a remainder)
             if n % num == 0:
                 factors.append(num)  # If so, add it to the list of factors
+            if len(factors) == k:
+                break
 
         # Check if there are at least k factors
-        if len(factors) >= k:
+        if len(factors) == k:
             # If yes, return the k-th factor (1-based index, so we use k-1 for 0-based indexing)
             return factors[k - 1]
         else:
