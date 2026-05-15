8b6a2673e899e9fcb84472d2ced09f6cfd51b52e
chuawz09082 <chuawz09082@gmail.com>
2026-05-14 21:22:33 -0700

    Time: 6 ms (99.37%) | Memory: 76.6 MB (10.82%) - LeetSync

---
+++ 1793-minimum-moves-to-make-array-complementary/minimum-moves-to-make-array-complementary.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minMoves(int[] nums, int limit) {
+        int n = nums.length;
+        int[] delta = new int[2*limit+2];
+
+        for (int i = 0; i < n/2; i++){
+            int min = Math.min(nums[i], nums[n - 1 - i]);
+            int max = Math.max(nums[i], nums[n - 1 - i]);
+
+            delta[2] += 2;
+            delta[min + 1]--;
+            delta[min + max]--;
+            delta[min + max + 1]++;
+            delta[max + limit + 1]++;
+        }
+
+
+        int res = n;
+        int moves = 0;
+
+        for (int targ = 2; targ <= limit * 2; targ++) {
+            moves += delta[targ];
+            res = Math.min(res, moves);
+        }
+
+        return res;
+        
+    }
+}
