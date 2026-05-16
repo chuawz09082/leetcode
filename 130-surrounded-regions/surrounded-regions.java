c9df79408da5085fe4b34b1734722bcc8dfc7562
chuawz09082 <chuawz09082@gmail.com>
2026-05-16 15:30:05 -0700

    Time: 2 ms (75.85%) | Memory: 47.3 MB (95.00%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -2,59 +2,44 @@ class Solution {
     private int[][] neighbours = {{1,0},{-1,0},{0,1},{0,-1}};
     private int rows;
     private int cols;
-    private List<List<Integer>> regionsO;
-    private boolean[][] visited;
+
 
     public void solve(char[][] board) {
         rows = board.length;
         cols = board[0].length;
-        
-        visited = new boolean[rows][cols];
 
-        for (int row = 1; row < rows-1; row++){
-            for (int col = 1; col < cols-1; col++){
-                if (board[row][col] == 'O' && !visited[row][col]){
-                    regionsO = new ArrayList<>();
-                    boolean toFlip = isIsland(row,col,board);
-                    if (toFlip){
-                        for (List<Integer> list: regionsO){
-                            board[list.get(0)][list.get(1)] = 'X';
-                        }
-                    }
-                }
-
-          
-            }
+        for (int i = 0; i < rows; i++){
+            if (board[i][0] == 'O') dfs(i,0,board);
+            if (board[i][cols-1] == 'O') dfs(i,cols-1,board);
         }
 
-        
+        for (int i = 0; i < cols; i++){
+            if (board[0][i] == 'O') dfs(0,i,board);
+            if (board[rows-1][i] == 'O') dfs(rows-1,i,board);
+        }
+
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < cols; j++){
+                if (board[i][j] == 'O') board[i][j] = 'X';
+                if (board[i][j] == 'T') board[i][j] = 'O';
+            }
+        }
         
     }
 
-    private boolean isIsland(int row, int col, char[][] board){
-        if (row == 0 || row == rows - 1 ||
-            col == 0 || col == cols - 1) {
-            return false;
-        }
-
-        visited[row][col] = true;
-        regionsO.add(List.of(row, col));
-        boolean ans = true;
+    private void dfs(int row, int col, char[][] board){
+        board[row][col] = 'T';
 
         for (int[] n: neighbours){
-            int newrow = row + n[0];
+            int newrow = row+ n[0];
             int newcol = col + n[1];
 
-            if (newrow < 0 || newrow >= rows || newcol < 0 || newcol >= cols || board[newrow][newcol] == 'X') continue;
+            if (newrow < 0 || newrow >= rows || newcol < 0 || newcol >= cols ||
+            board[newrow][newcol] != 'O') continue;
 
-            if (visited[newrow][newcol] == false ){
-                ans = isIsland(newrow, newcol, board) && ans;
-            }
-
-            
+            dfs(newrow,newcol,board);
         }
-
-
-        return ans;
+            
+      
     }
 }
