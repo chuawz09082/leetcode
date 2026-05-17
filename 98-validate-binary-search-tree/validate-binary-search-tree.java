682684b97c6704e8f0f8ed2ac30b43796df2eeb6
chuawz09082 <chuawz09082@gmail.com>
2026-05-17 15:57:51 -0700

    Time: 0 ms (100.00%) | Memory: 45.7 MB (6.62%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -19,10 +19,12 @@ class Solution {
     }
 
     private boolean dfs(TreeNode node, Integer min, Integer max){
-        if (node == null ) return true;
-
+        if (node == null) return true;
+        
         if ((min != null && node.val <= min) || (max != null && node.val >= max)) return false;
 
         return dfs(node.left, min, node.val) && dfs(node.right, node.val, max);
+
+        
     }
 }
